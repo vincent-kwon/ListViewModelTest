5cd6ede1c2a10fcb54b5f5336bd63715ae7880c9
hobum kwon <hobum.kwon@gmail.com>
2018-02-11 22:48:29 +0900

    Initial working version

---
+++ ListViewModelTest/ListViewModelTest/ListContainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewModelTest
+{
+    public class Contacts
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Num
+        {
+            get;
+            set;
+        }
+        public string imgsource
+        {
+            get;
+            set;
+        }
+    }
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListContainPage : ContentPage
+	{
+        //public ListViewModelView ViewModel
+        //{
+        //    get;
+        //    set;
+        //}
+        public ListContainPage()
+        {
+            InitializeComponent();
+            EmployeeView.ItemsSource = new List<Contacts>() {
+                new Contacts() {
+                        Name = "Umair", Num = "0456445450945", imgsource = "http://bit.ly/2oDQpPT",
+                },
+                new Contacts() {
+                    Name = "Cat", Num = "034456445905", imgsource = "http://gtty.im/2psFEos",
+                },
+                new Contacts() {
+                    Name = "Nature", Num = "56445905", imgsource = "http://gtty.im/2psFEos",
+                },
+            };
+            ListViewModelView lvl = new ListViewModelView("Bloomberg");
+            //ViewModel = lvl;
+            BindingContext = lvl;
+            lvl.Title = "This is it !!!!!!!!!!!!!!!";
+        }
+	}
+}
---
+++ ListViewModelTest/ListViewModelTest/ListViewModelView.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace ListViewModelTest
+{
+    public class ListViewModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+            }
+        }
+
+        public ListViewModelView(string title) 
+        {
+            Title = title;
+        }
+    }
+}
